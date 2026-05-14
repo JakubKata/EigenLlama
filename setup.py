db3e2aaca8746cc34e5cb9447a4fa887ac05a36b
JakubKata <jakubkata0@gmail.com>
2026-05-15 00:59:58 +0200

    Migrate to setup.py and pyproject.toml build system

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+ext_modules = [
+    Pybind11Extension(
+        "tiny_math",
+        ["core/LLMEngine.cpp"],
+        include_dirs=["core/lib/eigen"]
+    ),
+]
+
+setup(
+    name="tinyllama_engine",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+)
